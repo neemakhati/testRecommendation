64f3328f5df3771966d735ba3141c2233ce29565
Neema Khati <neemakhati11@gmail.com>
2023-06-05 14:37:37 +0545

    Recome

--- src/screens/RandomWorkshopScreen.js
+++ src/screens/RandomWorkshopScreen.js
@@ -1,53 +1,124 @@
 import React, { useState, useEffect } from 'react';
-import { ScrollView, View, Text, Button } from 'react-native';
+import { ScrollView, View, Text, Image, TouchableOpacity, StyleSheet } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { fetchEvents } from './api';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const RandomWorkshopScreen = () => {
-  const [randomWorkshop, setRandomWorkshop] = useState('');
+  const [randomWorkshops, setRandomWorkshops] = useState([]);
+  const [selectedWorkshops, setSelectedWorkshops] = useState([]);
   const navigation = useNavigation();
 
   useEffect(() => {
-    getRandomWorkshop();
+    getRandomWorkshops();
   }, []);
 
   const handleSelectWorkshop = (workshop) => {
     navigation.navigate('RecommendationScreen', { workshop });
   };
 
-  const getRandomWorkshop = async () => {
+  const getRandomWorkshops = async () => {
     try {
       const events = await fetchEvents();
       if (events.length > 0) {
-        const randomIndex = Math.floor(Math.random() * events.length);
-        const workshop = events[randomIndex]?.Workshop;
-        if (workshop) {
-          setRandomWorkshop(workshop);
+        const randomIndices = getRandomIndices(events.length, 20);
+        const workshops = randomIndices.map((index) => events[index]).filter(Boolean);
+        if (workshops.length > 0) {
+          setRandomWorkshops(workshops);
         } else {
-          console.log('Random workshop not found.');
+          console.log('Random workshops not found.');
         }
       } else {
         console.log('No workshops available.');
       }
     } catch (error) {
-      console.error('Error getting random workshop:', error);
+      console.error('Error getting random workshops:', error);
+    }
+  };
+
+  const getRandomIndices = (max, count) => {
+    const indices = [];
+    while (indices.length < count) {
+      const randomIndex = Math.floor(Math.random() * max);
+      if (!indices.includes(randomIndex)) {
+        indices.push(randomIndex);
+      }
+    }
+    return indices;
+  };
+
+  const isWorkshopSelected = (workshop) => {
+    return selectedWorkshops.includes(workshop);
+  };
+
+  const handleFavoriteWorkshop = (workshop) => {
+    const isSelected = selectedWorkshops.includes(workshop);
+    const updatedSelectedWorkshops = isSelected
+      ? selectedWorkshops.filter((selected) => selected !== workshop)
+      : [...selectedWorkshops, workshop];
+    setSelectedWorkshops(updatedSelectedWorkshops);
+
+    if (!isSelected) {
+      navigation.navigate('RecommendationScreen', { workshop });
     }
   };
 
   return (
-    <ScrollView>
-      <View>
-        <Text>Random Workshop:</Text>
-        <Text>{randomWorkshop}</Text>
-        <Button
-          title="Select as Favorite"
-          onPress={() => handleSelectWorkshop(randomWorkshop)}
-          disabled={!randomWorkshop}
-        />
-      </View>
+    <ScrollView contentContainerStyle={styles.container}>
+      {randomWorkshops.map((workshop, index) => (
+        <View key={index} style={styles.cardContainer}>
+          <Image source={{ uri: workshop.Images }} style={styles.image} resizeMode="contain" />
+          <View style={styles.textContainer}>
+            <Text style={styles.workshopText}>{workshop.Workshop}</Text>
+            <TouchableOpacity
+              style={styles.favoriteButton}
+              onPress={() => handleFavoriteWorkshop(workshop.Workshop)}
+            >
+              <Icon
+                name={isWorkshopSelected(workshop.Workshop) ? 'heart' : 'heart-outline'}
+                size={20}
+                color={isWorkshopSelected(workshop.Workshop) ? 'red' : '#333333'}
+              />
+            </TouchableOpacity>
+          </View>
+        </View>
+      ))}
     </ScrollView>
   );
 };
-//n
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 16,
+  },
+  cardContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#ffffff',
+    borderRadius: 8,
+    marginBottom: 16,
+    elevation: 2,
+    padding: 8,
+  },
+  image: {
+    width: 100,
+    height: 100,
+    borderRadius: 8,
+  },
+  textContainer: {
+    flex: 1,
+    marginLeft: 16,
+  },
+  workshopText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#333333',
+    marginBottom: 8,
+  },
+  favoriteButton: {
+    marginLeft: 'auto',
+    padding: 8,
+  },
+});
 
 export default RandomWorkshopScreen;
--- src/screens/RecommendationScreen.js
+++ src/screens/RecommendationScreen.js
@@ -1,5 +1,43 @@
+// import React, { useState, useEffect } from 'react';
+// import { ScrollView, Image, View, Text } from 'react-native';
+// import { getRecommendations } from './RecommendationService';
+
+// const RecommendationScreen = ({ route }) => {
+//   const { workshop } = route.params;
+//   const [recommendations, setRecommendations] = useState([]);
+
+//   useEffect(() => {
+//     const fetchRecommendations = async () => {
+//       const fetchedRecommendations = await getRecommendations(workshop);
+//       setRecommendations(fetchedRecommendations);
+//     };
+
+//     fetchRecommendations();
+//   }, [workshop]);
+
+//   return (
+//     <ScrollView>
+//     <View>
+//       <Text>Top recommendations for {workshop}:</Text>
+//       {recommendations.map((recommendation, index) => (
+//         <View key={index}>
+//           <Text>Workshop: {recommendation.Workshop}</Text>
+//           <Text>College: {recommendation.College}</Text>
+//           <Text>Ratings: {recommendation.Ratings}</Text>
+//           <Text>Description: {recommendation.Description}</Text>
+//           <Image source={{ uri: recommendation.Images }} style={{ width: 300, height: 120 }} />
+//         </View>
+//       ))}
+//     </View>
+//     </ScrollView>
+//   );
+// };
+
+// export default RecommendationScreen;
+
+
 import React, { useState, useEffect } from 'react';
-import { ScrollView, Image, View, Text } from 'react-native';
+import { ScrollView, Image, View, Text, StyleSheet } from 'react-native';
 import { getRecommendations } from './RecommendationService';
 
 const RecommendationScreen = ({ route }) => {
@@ -16,21 +54,48 @@ const RecommendationScreen = ({ route }) => {
   }, [workshop]);
 
   return (
-    <ScrollView>
-    <View>
-      <Text>Top recommendations for {workshop}:</Text>
-      {recommendations.map((recommendation, index) => (
-        <View key={index}>
-          <Text>Workshop: {recommendation.Workshop}</Text>
-          <Text>College: {recommendation.College}</Text>
-          <Text>Ratings: {recommendation.Ratings}</Text>
-          <Text>Description: {recommendation.Description}</Text>
-          <Image source={{ uri: recommendation.Images }} style={{ width: 300, height: 120 }} />
-        </View>
-      ))}
-    </View>
+    <ScrollView contentContainerStyle={styles.container}>
+      <View>
+        {recommendations.map((recommendation, index) => (
+          <View key={index} style={styles.cardContainer}>
+            <Text style={styles.workshopText}>Workshop: {recommendation.Workshop}</Text>
+
+            <Image source={{ uri: recommendation.Images }} style={styles.image} resizeMode="contain" />
+          </View>
+        ))}
+      </View>
     </ScrollView>
   );
 };
 
+const styles = StyleSheet.create({
+  container: {
+    padding: 16,
+  },
+  heading: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 16,
+  },
+  cardContainer: {
+    backgroundColor: '#ffffff',
+    borderRadius: 8,
+    marginBottom: 16,
+    elevation: 2,
+    padding: 16,
+  },
+  workshopText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 8,
+  },
+  image: {
+    width: '100%',
+    height: 120,
+    borderRadius: 8,
+    marginTop: 8,
+  },
+});
+
 export default RecommendationScreen;
+
